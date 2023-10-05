2fb76f85fe93540b01040516ab70747394fea8b8
hhm150456 <hagarhisham2004@example.com>
2023-10-05 11:46:32 -0700

    Commit 8

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - Entry point
+ *
+ * Description: A C program that displays the required sentence using the puts function
+ *
+ * Return: Always 0 (Success)
+ * /
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
+
