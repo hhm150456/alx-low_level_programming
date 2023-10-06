b0f29ee79ecbabaa6537e4eb4ddb63372928ac13
hhm150456 <hagarhisham2004@example.com>
2023-10-06 06:01:59 -0700

    Task 2 commit 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the base 10 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 48; i < 58; ++i)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
