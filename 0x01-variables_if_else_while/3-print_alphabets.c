4e9813f2426660f43143b28d3f2fa7c55799c019
hhm150456 <hagarhisham2004@example.com>
2023-10-06 02:49:30 -0700

    Task 2 commit 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the entire alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		++a;
+	}
+	a = 'A';
+	while (a <= 'Z')
+	{
+		putchar(a);
+		++a;
+	}
+	putchar('\n');
+	return (0);
+}
