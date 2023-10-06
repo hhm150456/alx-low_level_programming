b923dcfa1caf2970f9ed8d3ef6def3e228e825bb
hhm150456 <hagarhisham2004@example.com>
2023-10-06 05:54:39 -0700

    Task 2 commit 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the base 10 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 0; i < 10; ++i)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
