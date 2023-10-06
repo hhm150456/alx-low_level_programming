9590c5ebee43c3993bbbccfefebbeeb8b12a57d3
hhm150456 <hagarhisham2004@example.com>
2023-10-06 06:53:19 -0700

    Task 2 commit 13

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the base 10 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; ++i)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			continue;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
