3f71aaa24b63b0c40b7c5b3aac8f0acfe0bcf528
hhm150456 <hagarhisham2004@example.com>
2023-10-12 13:19:46 -0700

    TASK 4 commit 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiples a and b
+ * @a: n1
+ * @b: n1
+ *
+ * Return: 1 on success
+ * on error, -1 and error set appropriately
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
