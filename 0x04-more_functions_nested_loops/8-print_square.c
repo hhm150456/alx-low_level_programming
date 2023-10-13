b3ed79ced0dc632ea3d7cdc0a4698b77e2fa6f09
hhm150456 <hagarhisham2004@example.com>
2023-10-13 11:05:53 -0700

    TASK 4 commit 18

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: length of line
+ *
+ * Return: 1 on success
+ * on error, -1 and error set appropriately
+*/
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; ++i)
+	{
+		for (j = 0; j < size; ++j)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
