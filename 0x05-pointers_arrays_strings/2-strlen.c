5f60e758ab81002fa9d5ddaa5121d692c900921a
hhm150456 <hagarhisham2004@example.com>
2023-10-16 13:17:19 -0700

    task 5 commit 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - returns string length
+ * @s: the reference of string s
+ *
+ * Return: int  on success,
+ * -1 on error set appropriately
+*/
+
+int _strlen(char *s)
+{
+	int i, count;
+
+	count = 0;
+	for (i = 0; i < *s.size(); ++i)
+	{
+		++count;
+	}
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - returns string
+ * @str: the reference of string s
+ *
+ * Return: 1  on success,
+ * -1 on error set appropriately
+*/
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i < 63; ++i)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
