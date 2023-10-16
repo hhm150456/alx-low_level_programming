a8b1e380b13ae34b7d7ed229f3f8202316e40167
hhm150456 <hagarhisham2004@example.com>
2023-10-16 16:43:24 -0700

    task 5 commit 9

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - returns string
+ * @str: the reference of string s
+ *
+ * Return: 1  on success,
+ * -1 on error set appropriately
+*/
+
+void puts_half(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i])
+	{
+		++i;
+	}
+	for (j = (i - 1) / 2; j < i; ++j)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
